a287691228aaed31435127332a89d571b359ea5a
Anas-jaf <ansas4565@gmail.com>
2022-08-13 21:10:41 +0300

    restored somefiles

---
+++ flaskapp/app.py
@@ -0,0 +1,12 @@
+from flask import Flask , render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template('index1.html', loop=list(range(5)))
+
+if __name__ == '__main__':
+    # APP.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)
+    # return render_template('hello.html', name=name)
